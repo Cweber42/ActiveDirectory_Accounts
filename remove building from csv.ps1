0c1320335ebce524d1eeecafab0867a5dd5d657f
Cweb <xeniogame@gmail.com>
2018-03-23 09:52:31 -0500

    Example of how to filter the CSV
    This script shows you how you can eliminate and filter the Studentinfo CSV for other purposes you may have or need. This is helpful if you only want to create accounts for your a particular group of students, but do not want to make changes to the base studentinfo csv.

---
+++ remove building from csv.ps1
@@ -0,0 +1,3 @@
+﻿$csv = "C:\Users\techcoord\Favorites\Desktop\ACOT\studentinfo.csv"
+
+Import-Csv $csv | Where {$_.'Current Building' -ne "9"} | Export-Csv "C:\Users\techcoord\Favorites\Desktop\ACOT\studentinfo-hs.csv"
